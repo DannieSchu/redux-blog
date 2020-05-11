9f595b7ec876084132a5daf4c74371026e12dc6f
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-11 15:02:07 -0700

    write failing test for UPDATE_POST action

--- src/components/actions/postActions.test.js
+++ src/components/actions/postActions.test.js
@@ -1,4 +1,4 @@
-import { addPost, ADD_POST, deletePost, DELETE_POST } from './postActions';
+import { addPost, ADD_POST, deletePost, DELETE_POST, updatePost, UPDATE_POST } from './postActions';
 
 describe('post actions', () => {
   it('creates an ADD_POST action', () => {
@@ -22,6 +22,21 @@ describe('post actions', () => {
     expect(action).toEqual({
       type: DELETE_POST,
       payload: 8
+    });
+  });
+
+  it('creates an UPDATE_POST action', () => {
+    const action = updatePost({
+      id: '2tg32',
+      body: 'grwh vg00 jno noregr-rsib josbnvs nvldsd 0fijdosj'
+    });
+
+    expect(action).toEqual({
+      type: UPDATE_POST,
+      payload: {
+        id: '2tg32',
+        body: 'grwh vg00 jno noregr-rsib josbnvs nvldsd 0fijdosj'
+      }
     });
   });
 });
