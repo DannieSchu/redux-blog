11a224ae4603d5f63e1ff97c6b417f632d9c616b
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-11 14:51:35 -0700

    write passing DELETE_POST action

--- src/components/actions/postActions.js
+++ src/components/actions/postActions.js
@@ -4,6 +4,10 @@ export const addPost = post => ({
   payload: post
 });
 
-// delete post
+export const DELETE_POST = 'DELETE_POST';
+export const deletePost = index => ({
+  type: DELETE_POST,
+  payload: index
+});
 
 // update post (pass it an id and body)
