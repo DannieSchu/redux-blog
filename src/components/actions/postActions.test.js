b4d9d0dce2f09df8fc5f719616da3b1d29b3dadf
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-11 14:47:22 -0700

    write failing test for DELETE_POST action

--- src/components/actions/postActions.test.js
+++ src/components/actions/postActions.test.js
@@ -1,4 +1,4 @@
-import { addPost, ADD_POST } from './postActions';
+import { addPost, ADD_POST, deletePost, DELETE_POST } from './postActions';
 
 describe('post actions', () => {
   it('creates an ADD_POST action', () => {
@@ -13,6 +13,15 @@ describe('post actions', () => {
         title: 'something clever',
         body: 'q feqfneowi dwq h0gh390vn ifoheqwoi fhs0 hs dvlsd bvdjlguwe9ghewdsv vbvsjkjlge'
       }
+    });
+  });
+
+  it('creates a DELETE_POST action', () => {
+    const action = deletePost(8);
+
+    expect(action).toEqual({
+      type: DELETE_POST,
+      payload: 8
     });
   });
 });
